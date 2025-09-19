c51e9b4c8e006856f3b9a84dc4c0deecd80e6ea9
Boris Vasilenko <me@dreaktor.com>
2025-09-19 10:02:38 +0300

    feat(frontend): setup toast component

--- frontend/src/app/providers/providers.tsx
+++ frontend/src/app/providers/providers.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from './theme-provider'
 import type React from 'react'
 import messages from '@shared/lib/intl'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from '@shared/ui/sonner'
 
 const queryClient = new QueryClient()
 
@@ -10,6 +11,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => (
   <ThemeProvider>
     <IntlProvider locale="en" messages={messages.en}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <Toaster />
     </IntlProvider>
   </ThemeProvider>
 )
