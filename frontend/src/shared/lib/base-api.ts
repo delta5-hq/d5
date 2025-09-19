30d1dfd2cf6f62f5930b39d394c9981212bc42c6
Boris Vasilenko <me@dreaktor.com>
2025-09-19 10:02:37 +0300

    feat(frontend): handle multiple refresh requests

--- frontend/src/shared/lib/base-api.ts
+++ frontend/src/shared/lib/base-api.ts
@@ -1,24 +1,36 @@
-import { API_FULL_URL } from '@shared/config/api'
+import { API_BASE_PATH } from '@shared/config/api'
 import logger from './logger'
 
 interface ApiFetchOptions extends RequestInit {
   retry?: boolean
 }
 
-export const apiFetch = async <T = unknown>(url: string, options: ApiFetchOptions = {}): Promise<T> => {
-  const res = await fetch(`${API_FULL_URL}${url}`, options)
+let refreshPromise: Promise<Response> | null = null
 
-  if (res.status === 401 && !options.retry) {
-    const refreshRes = await fetch(`${API_FULL_URL}/refresh`, {
+async function refreshToken() {
+  if (!refreshPromise) {
+    refreshPromise = fetch(`${API_BASE_PATH}/refresh`, {
       method: 'POST',
       credentials: 'include',
+    }).finally(() => {
+      refreshPromise = null
     })
+  }
+
+  return refreshPromise
+}
+
+export const apiFetch = async <T = unknown>(url: string, options: ApiFetchOptions = {}): Promise<T> => {
+  const res = await fetch(`${API_BASE_PATH}${url}`, options)
+
+  if (res.status === 401 && !url.startsWith('/auth') && !options.retry) {
+    const refreshRes = await refreshToken()
 
     if (!refreshRes.ok) {
       throw new Error('Unauthorized, refresh failed')
     }
 
-    return apiFetch(url, { ...options, retry: true })
+    return apiFetch<T>(url, { ...options, retry: true })
   }
 
   if (!res.ok) {
@@ -34,5 +46,5 @@ export const apiFetch = async <T = unknown>(url: string, options: ApiFetchOption
     throw new Error(`HTTP ${res.status}: ${errorMessage}`)
   }
 
-  return res.json()
+  return res.json() as Promise<T>
 }
