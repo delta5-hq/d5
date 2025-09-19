2890d3e082865cdf4fb8b8926fb170a17148dfa4
Boris Vasilenko <me@dreaktor.com>
2025-09-19 10:02:36 +0300

    chore: setup steiger plugin

---
+++ frontend/steiger.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'steiger'
+import fsd from '@feature-sliced/steiger-plugin'
+
+export default defineConfig([
+  ...fsd.configs.recommended,
+  {
+    files: ['./src/shared/**'],
+    rules: {
+      'fsd/public-api': 'off',
+    },
+  },
+  {
+    // Allow cross-imports between widgets
+    files: ['src/widgets/**'],
+    rules: {
+      'fsd/no-cross-imports': 'off',
+      'fsd/insignificant-slice': 'off'
+    },
+  },
+])
