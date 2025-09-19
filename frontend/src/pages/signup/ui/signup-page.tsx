155626742b2f1eca627c61331413344cff63384b
Boris Vasilenko <me@dreaktor.com>
2025-09-19 10:02:39 +0300

    feat(frontend): signup page

--- frontend/src/pages/signup/ui/signup-page.tsx
+++ frontend/src/pages/signup/ui/signup-page.tsx
@@ -3,8 +3,7 @@ import { FormattedMessage } from 'react-intl'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod/v3'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useAuth, validateUsernameOrEmail, isValidPassword } from '@entities/auth'
-import { LoginButton } from '@widgets/app-layout/ui/header'
+import { useAuth, validateUsernameOrEmail, isValidPassword, LoginDialog } from '@entities/auth'
 import { useNavigate } from 'react-router-dom'
 import { Button } from '@shared/ui/button'
 import { Input } from '@shared/ui/input'
@@ -13,6 +12,7 @@ import { isEmail } from '@shared/lib/email'
 import { Version } from '@shared/ui/version'
 import { Copyright } from '@shared/ui/copyright'
 import { Logo } from '@shared/ui/logo'
+import { Label } from '@shared/ui/label'
 
 const signupSchema = z.object({
   username: z
@@ -76,7 +76,7 @@ const Signup: React.FC = () => {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen relative bg-gray-50">
+    <div className="flex justify-center items-center h-screen relative bg-muted">
       <div className="absolute top-5 left-5">
         <Logo />
       </div>
@@ -91,91 +91,90 @@ const Signup: React.FC = () => {
       ) : null}
       {!isSuccess ? (
         <div className="bg-white shadow-md rounded-xl p-6 w-full max-w-sm sm:max-w-md">
-          <form noValidate onSubmit={handleSubmit(onSignUp)}>
-            <h2 className="text-xl font-semibold mb-4">
+          <form className="flex flex-col gap-y-3" noValidate onSubmit={handleSubmit(onSignUp)}>
+            <h2 className="text-xl font-semibold">
               <FormattedMessage id="signupTitle" />
             </h2>
 
             {/* Username */}
-            <div className="mb-3">
-              <label className="block text-sm font-medium text-gray-700" htmlFor="username">
+            <div>
+              <Label className="block text-sm font-medium text-foreground" htmlFor="username">
                 <FormattedMessage id="username" />
-              </label>
+              </Label>
               <Input
                 autoComplete="username"
                 autoFocus
                 id="username"
                 type="text"
                 {...register('username')}
-                className={`mt-1 block w-full rounded-md border ${
-                  errors.username ? 'border-red-500' : 'border-gray-300'
+                className={`block w-full rounded-md border ${
+                  errors.username ? 'border-destructive' : 'border-muted'
                 } p-2`}
               />
               {errors.username ? (
-                <p className="text-red-500 text-sm mt-1">
+                <p className="text-destructive text-sm">
                   <FormattedMessage id={errors.username.message as string} />
                 </p>
               ) : null}
             </div>
 
             {/* Email */}
-            <div className="mb-3">
-              <label className="block text-sm font-medium text-gray-700" htmlFor="mail">
+            <div>
+              <Label className="block text-sm font-medium text-foreground" htmlFor="mail">
                 <FormattedMessage id="email" />
-              </label>
+              </Label>
               <Input
                 autoComplete="mail"
                 id="mail"
                 type="email"
                 {...register('mail')}
-                className={`mt-1 block w-full rounded-md border ${
-                  errors.mail ? 'border-red-500' : 'border-gray-300'
-                } p-2`}
+                className={`block w-full rounded-md border ${errors.mail ? 'border-destructive' : 'border-muted'} p-2`}
               />
               {errors.mail ? (
-                <p className="text-red-500 text-sm mt-1">
+                <p className="text-destructive text-sm">
                   <FormattedMessage id={errors.mail.message as string} />
                 </p>
               ) : null}
             </div>
 
             {/* Password */}
-            <div className="mb-3">
-              <label className="block text-sm font-medium text-gray-700" htmlFor="password">
+            <div>
+              <Label className="block text-sm font-medium text-foreground" htmlFor="password">
                 <FormattedMessage id="password" />
-              </label>
+              </Label>
               <Input
                 autoComplete="current-password"
                 id="password"
                 type="password"
                 {...register('password')}
-                className={`mt-1 block w-full rounded-md border ${
-                  errors.password ? 'border-red-500' : 'border-gray-300'
+                className={`block w-full rounded-md border ${
+                  errors.password ? 'border-destructive' : 'border-muted'
                 } p-2`}
               />
               {errors.password ? (
-                <p className="text-red-500 text-sm mt-1">
+                <p className="text-destructive text-sm">
                   <FormattedMessage id={errors.password.message as string} />
                 </p>
               ) : null}
             </div>
 
             {/* Already have account */}
-            <div className="mt-4 text-center text-sm">
-              <FormattedMessage id="alreadyExistAccount" /> <LoginButton login={login} />
+            <div className="flex gap-x-2 justify-center text-center text-sm">
+              <FormattedMessage id="alreadyExistAccount" />{' '}
+              <LoginDialog login={login}>
+                <span className="cursor-pointer hover:underline hover:text-link-hover text-link">
+                  <FormattedMessage id="loginTitle" />
+                </span>
+              </LoginDialog>
             </div>
 
             {/* Version */}
-            <div className="mt-6 text-center text-xs text-gray-500">
+            <div className="text-center text-xs text-foreground/40">
               Version <Version /> - <Copyright />
             </div>
 
-            <div className="mt-6 flex justify-between">
-              <Button
-                className="px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300 text-gray-800"
-                onClick={() => navigate(-1)}
-                type="button"
-              >
+            <div className="flex justify-between">
+              <Button className="px-4 py-2 rounded-md" onClick={() => navigate(-1)} type="button">
                 <FormattedMessage id="buttonCancel" />
               </Button>
               <Button
